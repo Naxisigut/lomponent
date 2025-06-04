51be861b0e41b2998d49b12e6d498c5202c0997c
Naxisigut <834525217@qq.com>
2025-06-04 16:21:19 +0800

    feat: 添加自定义Tabbar组件及相关文档

---
+++ CustomTabbar/README.md
@@ -0,0 +1,25 @@
+## 简介
+自定义Tabbar。
+
+文件夹中的`uu-tabbar`组件是从`vk-uview-ui`(支持vue2和vue3)组件库中的Tabbar组件复制、修改而来
+```js
+// main.js
+import CustomTabbar from '@/components/CustomTabbar/index.vue';
+app.component("RecycleMpTabbar", CustomTabbar)
+
+```
+
+```html
+<CustomTabbar></CustomTabbar>
+```
+
+## 平台与兼容性
+Uniapp 支付宝小程序
+
+## 技术栈
+vue3
+
+## 库依赖
+vk-uview-ui
+`https://vkuviewdoc.fsq.pub/components/intro.html`
+
---
+++ CustomTabbar/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <UUTabbar :list="list" @change="change" @mid="onMid"></UUTabbar>
+</template>
+
+<script setup lang='ts'>
+import UUTabbar from './uu-tabbar.vue'
+import { reactive } from 'vue'
+defineOptions({
+  name: 'CustomTabbar'
+})
+const emit = defineEmits(['change', 'mid'])
+
+const list = reactive([
+  {
+    iconPath: "/static/tabbar/home.png",
+    selectedIconPath: "/static/tabbar/home-active.png",
+    text: '首页',
+    customIcon: false,
+    pagePath: '/pages/index/index'
+  },
+  {
+    iconPath: "/static/tabbar/mid.png",
+    selectedIconPath: "/static/tabbar/mid.png",
+    // iconPath: "https://file-recycle.kuarke.com/data/product/1/20250520102106/20250520102106095_19183.png",
+    // selectedIconPath: "https://file-recycle.kuarke.com/data/product/1/20250520102106/20250520102106095_19183.png",
+    text: 'AI估价',
+    midButton: true,
+    customIcon: false,
+  },
+  {
+    iconPath: "/static/tabbar/my.png",
+    selectedIconPath: "/static/tabbar/my-active.png",
+    text: '我的',
+    customIcon: false,  
+    pagePath: '/pages/my/my'
+  },
+])
+
+const change = (params) => emit('change', params)
+const onMid = (params) => {
+  emit('mid', params)
+
+  /* 自定义中间按钮操作 */
+  // uni.chooseImage({
+  //   count: 1,
+  //   sizeType: ['compressed'],
+  //   extension: ['jpg', 'png', 'jpeg'],
+  //   success: (res) => {
+  //     console.log(res, 'res')
+  //     const tempFile = res.tempFiles[0]
+  //     if(tempFile) {
+  //       uni.setStorageSync('AiImage', tempFile)
+  //       uni.navigateTo({
+  //         url: '/packageA/pages/estimateBegin/estimateBegin?from=midButton'
+  //       })
+  //     }
+  //   },
+  //   fail: () => {
+  //     console.log('上传图片失败');
+  //   }
+  // })
+
+
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
